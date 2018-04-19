686a105dda5747010b0cdf6c07603408df92b807
Ed Charbeneau <ed.charbeneau@gmail.com>
2018-04-19 15:40:05 -0400

    live site

---
+++ _content/MarkdownComponent/htmlRendererInterop.js
@@ -0,0 +1,11 @@
+﻿// This file is to show how a library package may provide JavaScript interop features
+// wrapped in a .NET API
+
+Blazor.registerFunction('MarkdownComponent.HtmlRendererInterop.RenderMarkdown', function (message) {
+   
+    let el = document.getElementById("markdown-component");
+    if (el) { el.innerHTML = message; }
+    else {
+        console.log("HTML not rendered");
+    }
+});
